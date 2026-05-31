d54534e134cefd6cb25fce70d42820160b55b211
BHHP2026 <info@breusa.com>
2026-05-31 14:58:00 -0300

    Fix formatting in specs and add reorder function
    Bluffton reorder: 37 5th first, 239 Belfair second, 38 Newberry last

--- listings-update.js
+++ listings-update.js
@@ -19,7 +19,7 @@
       find:  '30 Heathrow Avenue',
       price: '$1,499,000',
       addr:  '239 Belfair Oaks Boulevard',
-      specs: '4 BD  ·  4 BA  ·  4,092 SF',
+      specs: '4 BD  .  4 BA  .  4,092 SF',
       img:   'https://d25fhp1qfwqa2h.cloudfront.net/fdd36be12f1450236f3a1c7b9cdaa488,1780023360603_auto_650',
       url:   'https://search.besthiltonheadproperties.com/search/detail/264812806?s[orderBy]=sourceCreationDate%2Cdesc&s[page]=1&s[locations][0][city]=Bluffton&s[locations][0][state]=SC',
     },
@@ -27,11 +27,17 @@
       find:  '16 Baldwin Lane',
       price: '$600,000',
       addr:  '37 5th Avenue',
-      specs: '3 BD  ·  3 BA  ·  2,088 SF',
+      specs: '3 BD  .  3 BA  .  2,088 SF',
       img:   'https://d25fhp1qfwqa2h.cloudfront.net/d3d83848eccd4f62228e8379ddba076b,1778250772663_auto_650',
       url:   'https://search.besthiltonheadproperties.com/search/detail/263820475?s[orderBy]=sourceCreationDate%2Cdesc&s[page]=5&s[locations][0][city]=Bluffton&s[locations][0][state]=SC',
     },
   ];
+
+  var BLUFFTON_ORDER = {
+    first:  '37 5th Avenue',
+    second: '239 Belfair Oaks',
+    last:   '38 Newberry Court',
+  };
 
   function applyReplacements() {
     var cards = document.querySelectorAll('.hhi-card');
@@ -62,11 +68,36 @@
     return applied > 0;
   }
 
+  function reorderBluffton() {
+    var tracks = document.querySelectorAll('.hhi-track');
+    if (tracks.length < 2) return false;
+    var track = tracks[1];
+    var cards = Array.from(track.querySelectorAll('.hhi-card'));
+    if (!cards.length) return false;
+    function addrOf(card) {
+      var el = card.querySelector('.hhi-card-addr');
+      return el ? el.textContent.trim().toLowerCase() : '';
+    }
+    var firstCard  = cards.find(function(c){ return addrOf(c).indexOf(BLUFFTON_ORDER.first.toLowerCase())  !== -1; });
+    var secondCard = cards.find(function(c){ return addrOf(c).indexOf(BLUFFTON_ORDER.second.toLowerCase()) !== -1; });
+    var lastCard   = cards.find(function(c){ return addrOf(c).indexOf(BLUFFTON_ORDER.last.toLowerCase())   !== -1; });
+    if (!firstCard && !secondCard && !lastCard) return false;
+    if (lastCard)   track.appendChild(lastCard);
+    var ref = track.firstChild;
+    if (secondCard) track.insertBefore(secondCard, ref);
+    if (firstCard)  track.insertBefore(firstCard,  track.firstChild);
+    return true;
+  }
+
   function run() {
-    if (applyReplacements()) return;
+    var r1 = applyReplacements();
+    var r2 = reorderBluffton();
+    if (r1 || r2) return;
     var attempts = 0;
     var timer = setInterval(function () {
-      if (applyReplacements() || ++attempts >= 15) clearInterval(timer);
+      var a = applyReplacements();
+      var b = reorderBluffton();
+      if ((a || b) || ++attempts >= 15) clearInterval(timer);
     }, 300);
   }
 
